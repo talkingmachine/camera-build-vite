ee467f46b31eeaf4b38669183b4a34996f99ac52
talkingmachine <aeyueoq@gmail.com>
2023-10-05 21:17:54 +0300

    added axios

---
+++ src/services/api-requests.ts
@@ -0,0 +1,20 @@
+// import { isRejectedWithValue } from '@reduxjs/toolkit';
+// import { BookingData } from '../types/types';
+// import createAPI from './api-axios';
+// import APIRoutes from '../consts/api-routes';
+
+// const api = createAPI();
+
+// const postBookingData = async (payload: BookingData, id: string) => {
+//   try {
+//     const {data} = await api.post<BookingData>(APIRoutes.PostBookingData(id), payload);
+//     return data;
+//   } catch (err) {
+//     return isRejectedWithValue(err);
+//   }
+// };
+
+
+// export {postBookingData, removeReservation};
+
+// нам это все понадобится чуть позже
