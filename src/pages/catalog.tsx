588fcb1c7a536b3ea7e4be0d40c4dd65721457a6
talkingmachine <aeyueoq@gmail.com>
2023-10-05 03:44:31 +0300

    double pagination fix

--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -61,18 +61,6 @@ export function CatalogPage ():JSX.Element {
                     </form>
                   </div>
                   <CatalogCards/>
-                  <div className="pagination">
-                    <ul className="pagination__list">
-                      <li className="pagination__item"><a className="pagination__link pagination__link--active" href="1">1</a>
-                      </li>
-                      <li className="pagination__item"><a className="pagination__link" href="2">2</a>
-                      </li>
-                      <li className="pagination__item"><a className="pagination__link" href="3">3</a>
-                      </li>
-                      <li className="pagination__item"><a className="pagination__link pagination__link--text" href="2">Далее</a>
-                      </li>
-                    </ul>
-                  </div>
                 </div>
               </div>
             </div>
