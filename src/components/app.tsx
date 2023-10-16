cef0bcaa1c1dd3b9a3098553c9a8a9448f4b5327
talkingmachine <aeyueoq@gmail.com>
2023-10-16 12:51:45 +0300

    not found page added

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,6 +3,7 @@ import { CatalogPage } from '../pages/catalog';
 import { RouterPaths } from '../consts/router-paths';
 import { ProductPage } from '../pages/product';
 import { Modal } from './popups/modal';
+import { NotFound } from '../pages/not-found';
 
 export function App ():JSX.Element {
   return (
@@ -10,6 +11,7 @@ export function App ():JSX.Element {
       <Routes>
         <Route path={RouterPaths.catalog} element={<CatalogPage/>} />
         <Route path={`${RouterPaths.product}/:id`} element={<ProductPage/>} />
+        <Route path={RouterPaths.notFound} element={<NotFound/>}/>
       </Routes>
       <Modal/>
     </BrowserRouter>
