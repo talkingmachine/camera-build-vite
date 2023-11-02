bd5898532654aa96bb13041410b52810cb572286
talkingmachine <aeyueoq@gmail.com>
2023-11-02 03:47:29 +0300

    Added header search

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,8 @@
 import { Link } from 'react-router-dom';
 import { RouterPaths } from '../../consts/router-paths';
-import { IconClose } from '../icon-components/icon-close';
-import { HeaderSelectList } from './header-select-list';
-import { useAppSelector } from '../../hooks/typed-wrappers';
+import { HeaderFormSearch } from './header-form-search';
 
 export function Header ():JSX.Element {
-
-  const products = useAppSelector((state) => state.DATA.productsList);
 
   return (
     <header className="header" id="header">
@@ -29,21 +25,7 @@ export function Header ():JSX.Element {
             </li>
           </ul>
         </nav>
-        <div className="form-search">
-          <form>
-            <label>
-              <svg className="form-search__icon" width={16} height={16} aria-hidden="true">
-                <use xlinkHref="#icon-lens" />
-              </svg>
-              <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" />
-            </label>
-            <HeaderSelectList selectListData={products}/>
-          </form>
-          <button className="form-search__reset" type="reset">
-            <IconClose/>
-            <span className="visually-hidden">Сбросить поиск</span>
-          </button>
-        </div>
+        <HeaderFormSearch/>
         <a className="header__basket-link" href="#">
           <svg width={16} height={16} aria-hidden="true">
             <use xlinkHref="#icon-basket" />
---
+++ src/utils/get-filtered-products.ts
@@ -0,0 +1,15 @@
+import { ProductData } from '../types/data-types';
+
+export const getFilteredProducts = (array: ProductData[], searchText: string) => {
+  const searchKeywords = searchText.split(' ').filter((keyword) => keyword);
+  return (
+    array.filter((product) => {
+      for (const keyword of searchKeywords) {
+        if (product.name.toLowerCase().includes(keyword.toLowerCase())) {
+          return true;
+        }
+      }
+      return false;
+    })
+  );
+};
